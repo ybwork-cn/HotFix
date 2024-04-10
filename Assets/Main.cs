a49a20075fed9a8a5ae662977962fd2a95514911
月北是个程序员 <34906711+ybwork-cn@users.noreply.github.com>
2024-04-10 14:44:24 +0800

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+public class HotFixAttribute : Attribute
+{
+    public readonly string Name;
+
+    public HotFixAttribute() { }
+
+    public HotFixAttribute(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log(Add(1, 2));
+    }
+
+    [HotFix]
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
